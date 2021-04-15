08d106a3c1de01e03b0f06a86582577f14b9edb5
Aleksey Yesin <esinru@gmail.com>
2021-04-15 16:27:19 +0300

    Adds pages lazy loading

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,27 @@
+import { lazy, Suspense } from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
 // import './styles/App.module.scss';
 
 import routes from './routes';
 import AppBar from './components/AppBar';
-import HomePage from './pages/HomePage';
-import MoviesPage from './pages/MoviesPage';
-import MovieDetailsPage from './pages/MovieDetailsPage';
+
+const HomePage = lazy(() => import('./pages/HomePage'));
+const MoviesPage = lazy(() => import('./pages/MoviesPage'));
+const MovieDetailsPage = lazy(() => import('./pages/MovieDetailsPage'));
 
 const App = () => (
     <AppBar>
-        <Switch>
-            <Route path={routes.home} exact component={HomePage} />
-            <Route path={routes.movieDetails} component={MovieDetailsPage} />
-            <Route path={routes.movies} component={MoviesPage} />
-            <Redirect to="/" />
-        </Switch>
+        <Suspense fallback={<div>Загрузка...</div>}>
+            <Switch>
+                <Route path={routes.home} exact component={HomePage} />
+                <Route
+                    path={routes.movieDetails}
+                    component={MovieDetailsPage}
+                />
+                <Route path={routes.movies} component={MoviesPage} />
+                <Redirect to="/" />
+            </Switch>
+        </Suspense>
     </AppBar>
 );
 
